38a57e1c3a82813cbc96341dda5d6d7c34aff941
pablopues <arovichpablo@gmail.com>
2022-01-04 21:59:39 -0300

    fisrt

---
+++ app.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.set('port',process.env.PORT || 3000)
+
+app.listen(app.get('port'),()=> console.log('on http://localhost:'+app.get('port')));
+
+app.get('/', (req,res)=> res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
