1cffae4ca5a43ed14093918fe6bf8326d888a92a
pablopues <arovichpablo@gmail.com>
2022-01-04 23:16:49 -0300

    2

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.use(express.static('public'));
 
 app.set('port',process.env.PORT || 3000)
 
-app.listen(app.get('port'),()=> console.log('on http://localhost:'+app.get('port')));
+app.listen(app.get('port'), ()=> {console.log('on http://localhost:'+app.get('port'));
+})
 
 app.get('/', (req,res)=> res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
